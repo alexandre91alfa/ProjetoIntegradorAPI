64b05714a5f5681ba71ed788c83fa68ff886c7a3
Alexandre <alexandre.alfa@yahoo.com>
2019-11-16 13:16:23 -0300

    adicionado model

---
+++ model/staff.cs
@@ -0,0 +1,12 @@
+
+namespace univespApiPI.Controllers
+{
+    class Staff
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string function { get; set; }
+        public int rg { get; set; }
+        public string imgUrl { get; set; }
+    }
+}
