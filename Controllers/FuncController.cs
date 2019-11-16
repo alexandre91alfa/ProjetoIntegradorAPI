c6decf9038aec2f20fde90af06832ce10b35452c
Alexandre <alexandre.alfa@yahoo.com>
2019-11-16 13:06:27 -0300

    univesp api inicial commit

---
+++ Controllers/FuncController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace univespApiPI.Controllers
+{
+    [Route("v1")]
+    class FuncController : Controller
+    {
+
+        FuncController()
+        {
+        }
+
+        // public async Task<IActionResult> GetFuncionarios()
+        // {
+        //     try
+        //     {
+        //         var result = await
+        //     }
+        //     catch (System.Exception)
+        //     {
+
+        //         throw;
+        //     }
+        // }
+    }
+}
